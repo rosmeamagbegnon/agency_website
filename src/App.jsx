3ff454423c32c1618bd025e9410419a85a6b515e
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2024-09-25 18:13:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+    return ( 
+        < >
+            <h1 className="font-bold text-2xl 2xl:text-5xl leading-7 bg-red-500">Hello J{"'"}apprends Reactjs</h1>
+              <div className="">
+                <p className="px-4 pt-8">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui vero soluta possimus quam ut necessitatibus harum ipsum exercitationem placeat dicta? Distinctio adipisci rem, cumque rerum accusamus saepe ipsum reprehenderit officiis.</p>
+              </div>
+        </>
+     );
+}
+ 
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server:{
+   host:'192.168.1.151',
+   port:3000
+  },
+  plugins: [react()],
+})
+
