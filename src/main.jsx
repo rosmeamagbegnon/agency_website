2c46cc6d0373cf5758dd0fa125fd17d8173478b7
Elcy <ichmellatognide@gmail.com>
2024-09-26 19:43:06 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+
 
 import './index.css'
 import HeroSection from './composants/HeroSection'
+import Footer from './composants/footer';
+import Caring from './composants/caring';
+import Header from './composants/Header';
+import Section from './composants/Section';
+import Clients from './composants/Clients';
+import Management from './composants/Management';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+    <Header/>
+    <Section/>
+    <Clients/>
+    <Management/>
+    <Caring/>
     <HeroSection/>
+    <Footer/>
   </StrictMode>,
 )
