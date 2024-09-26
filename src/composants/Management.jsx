223bd99e796d6ace99cd97ea4d5cad71a70e9262
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2024-09-26 18:59:51 +0100

    first commit

---
+++ src/composants/Management.jsx
@@ -0,0 +1,45 @@
+const Management = () => {
+    return (  
+        <section className="bg-white pb-2">
+            <div className="text-center">
+                <h1 className="text-black text-2xl  pb-1 font-semibold">Manage your entire community</h1>
+                <h1 className="text-black text-2xl  pb-1 font-semibold">in a single system</h1>
+                <h2 className="text-gray-400 text-xs pt-1 pb-8">Who is Nextcent suitable for?</h2>
+            </div>
+            <div className="justify-center gap-20 flex flex-wrap">
+                <div className="text-center">
+                    <div className="pl-24  ">
+                        <img className=" h-7  bg-green-100 " src="/src/assets/F20.png" alt="Hello" />
+                    </div>
+                    <h1 className="text-black text-xl font-semibold">Membership</h1>
+                    <h1 className="text-black text-xl pb-1 font-semibold">Organisations</h1>
+                    <h2 className="text-gray-400 text-xs ">Our membership management </h2>
+                    <h2 className="text-gray-400 text-xs ">software provides full automation of  </h2>
+                    <h2 className="text-gray-400 text-xs ">membership renewals and payments</h2>
+                </div>
+                <div className="text-center">
+                    <div className="pl-24">
+                        <img className=" h-7 bg-green-100" src="/src/assets/F21.png" alt="Hello" />
+                    </div>
+                    <h1 className="text-black text-xl font-semibold">National</h1>
+                    <h1 className="text-black text-xl pb-1 font-semibold">Associations</h1>
+                    <h2 className="text-gray-400 text-xs ">Our membership management </h2>
+                    <h2 className="text-gray-400 text-xs ">software provides full automation of  </h2>
+                    <h2 className="text-gray-400 text-xs ">membership renewals and payments</h2>
+                </div>
+                <div className="text-center">
+                    <div className="pl-24">
+                        <img className=" h-7 bg-green-100" src="/src/assets/F20.png" alt="Hello" />
+                    </div>
+                    <h1 className="text-black text-xl font-semibold">Clubs And</h1>
+                    <h1 className="text-black text-xl pb-1 font-semibold">Groups</h1>
+                    <h2 className="text-gray-400 text-xs ">Our membership management </h2>
+                    <h2 className="text-gray-400 text-xs ">software provides full automation of  </h2>
+                    <h2 className="text-gray-400 text-xs ">membership renewals and payments</h2>
+                </div>
+            </div>
+        </section>
+    );
+}
+ 
+export default Management;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
-
 import './index.css'
-import HeroSection from './composants/HeroSection'
 
+import Management from './composants/Management'
+import Clients from './composants/Clients'
+import Header from './composants/Header'
+import Section from './composants/Section'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
-    <HeroSection/>
+    <Header/>
+    <Section/>
+    <Clients/>
+    <Management/>
   </StrictMode>,
 )
