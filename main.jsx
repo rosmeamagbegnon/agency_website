a708cff604e5aa628761970621483c58caf0a7f0
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2024-09-28 20:33:24 +0100

    Correct

--- main.jsx
+++ main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import Management from '../Management'
-import Clients from '../Clients'
+import Management from './Management'
+import Clients from './Clients'
 import Header from '../composants/Header'
-import Section from '../Section'
-import Unseen from '../Unseen'
-import Helping from '../Helping'
+import Section from './Section'
+import Unseen from './Unseen'
+import Helping from './Helping'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
