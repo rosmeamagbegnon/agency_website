93877039a50fb6f08c9a8a9450b69a4df88841a6
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2024-09-28 20:28:30 +0100

    Moving

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import Management from './composants/Management'
-import Clients from './composants/Clients'
-import Header from './composants/Header'
-import Section from './composants/Section'
-import Unseen from './composants/Unseen'
-import Helping from './composants/Helping'
+import Management from '../Management'
+import Clients from '../Clients'
+import Header from '../composants/Header'
+import Section from '../Section'
+import Unseen from '../Unseen'
+import Helping from '../Helping'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
