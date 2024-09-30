46b010629d7aae543edf7c5b630fd0f189095fcc
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2024-09-30 08:56:27 +0100

    Repair

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,10 @@ import Smith from './composants/smith'
 import Design from './composants/design'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Design/> 
-   <Smith/>  
-   <Caring/>
-  <HeroSection/>
-  <Footer/>
+    <Design/> 
+    <Smith/>  
+    <Caring/>
+    <HeroSection/>
+    <Footer/>
   </StrictMode>,
 )
