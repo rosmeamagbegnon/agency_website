3f002a5189b06c5adf114e3a70e1fabc809dbd0f
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2024-09-30 16:17:14 +0100

    update

--- src/app.jsx
+++ src/app.jsx
@@ -5,10 +5,12 @@ import Clients from './composants/Clients';
 import Management from './composants/Management';
 import Unseen from './composants/Unseen';
 import Helping from './composants/Helping';
+import Blog from './composants/Blog';
 
 const App = () => {
   return (
     <div>
+      <Blog/>
       <Header />
       <Section />
       <Clients />
--- src/composants/Clients.jsx
+++ src/composants/Clients.jsx
@@ -5,7 +5,7 @@ const Clients = () => {
                 <h1 className="text-black text-2xl md:text-3xl 2xl:text-4xl pb-1 font-semibold">Our Clients</h1>
                 <h2 className="text-gray-400 text-xs 2xl:text-lg  2xl:font-semibold pb-6 2xl:pb-10">We have been working with some Fortune 500+ clients</h2>
             </div>
-            <div className="justify-center flex flex-wrap gap-x-20 2xl:gap-x-48 gap-y-8">
+            <div className="justify-center flex flex-wrap gap-x-16 lg:gap-x-20 xl:gap-x-36 gap-y-8">
                 <img className="h-8 2xl:h-12" src="/src/assets/F5.png" alt="Hello" />
                 <img className="h-8 2xl:h-12" src="/src/assets/F6.png" alt="Hello" />
                 <img className="h-8 2xl:h-12" src="/src/assets/F7.png" alt="Hello" />
